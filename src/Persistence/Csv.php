pwFoo/data-1

throw for explicit table everywhere

src/Persistence/Csv.php
@@ -343,7 +343,7 @@ class Csv extends Persistence
      * @param mixed $id
      * @param array $data
      */
-    public function update(Model $m, $id, $data, string $table = null)
+    public function update(Model $m, $id, $data)
     {
         throw new Exception('Updating records is not supported in CSV persistence.');
     }
@@ -353,7 +353,7 @@ class Csv extends Persistence
      *
      * @param mixed $id
      */
-    public function delete(Model $m, $id, string $table = null)
+    public function delete(Model $m, $id)
     {
         throw new Exception('Deleting records is not supported in CSV persistence.');
     }
@@ -367,6 +367,10 @@ class Csv extends Persistence
      */
     public function generateNewId($m, string $table = null)
     {
+        if ($table !== null) {
+            throw new \Error('debug!!');
+        }
+
         if ($table === null) {
             $table = $m->table;
         }

atk4/dataset

src/Persistence/Csv.php
@@ -336,7 +336,7 @@ class Csv extends Persistence
      *
      * @param mixed $id
      */
-    public function update(Model $model, $id, array $data, string $table = null)
+    public function update(Model $model, $id, array $data)
     {
         throw new Exception('Updating records is not supported in CSV persistence.');
     }
@@ -346,7 +346,7 @@ class Csv extends Persistence
      *
      * @param mixed $id
      */
-    public function delete(Model $model, $id, string $table = null)
+    public function delete(Model $model, $id)
     {
         throw new Exception('Deleting records is not supported in CSV persistence.');
     }
@@ -358,6 +358,10 @@ class Csv extends Persistence
      */
     public function generateNewId(Model $model, string $table = null)
     {
+        if ($table !== null) {
+            throw new \Error('debug!!');
+        }
+
         if ($table === null) {
             $table = $model->table;
         }